9c4221a7bf271bdf77bb4fd2bdc9b6bee14256a9
Daniel Andrews <daniel.raymond.andrews@gmail.com>
2012-07-08 20:03:09 -0600

    delete update_email.py

--- update_email.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-fromaddr = 'daniel.raymond.andrews@gmail.com'  
-toaddrs  = 'daniel.raymond.andrews+zetakap@gmail.com'
-USERNAME = 'daniel.raymond.andrews@gmail.com'  
-PASSWORD = 'red_asdra3'  
-
-  
