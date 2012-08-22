2d81b85201300d4a2c19e8f48cee4b065e13a882
Daniel Andrews <daniel.raymond.andrews@gmail.com>
2012-08-21 21:18:19 -0600

    make article file names match article titles

--- zetakap.py
+++ zetakap.py
@@ -6,8 +6,8 @@ import glob,os
 
 pwd = os.path.dirname(__file__)
 article_path = pwd + "/Articles"
-
-num_articles = len(glob.glob( article_path + "/Article_*.txt"))
+article_prefix = "a"
+num_articles = len(glob.glob( article_path + "/" + article_prefix + "*.txt"))
 articles = []
 
 while len( articles ) < 10:
@@ -22,7 +22,7 @@ while len( articles ) < 10:
         articles.append( article )
         article.tickerlist.update_pending_tickers()
         num_articles = num_articles + 1
-        article_file = open( article_path + '/Article_' + str(num_articles) + '.txt', 'w' )
+        article_file = open( article_path + '/' + article_prefix + str(num_articles) + "___" + article.title.replace(' ','_') + '.txt', 'w' )        
         article_file.write( str( article.print_article() ) )
         article_file.close()
     
