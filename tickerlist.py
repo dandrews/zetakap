7682eeb79fca97aa111d05dc57803bcaae4d7cf2
Daniel Andrews <daniel.raymond.andrews@gmail.com>
2012-08-29 19:51:02 -0600

    added more stocks to excluded list

--- tickerlist.py
+++ tickerlist.py
@@ -12,7 +12,10 @@ import os
 
 pwd = os.path.dirname(__file__)
 article_path = pwd + "/Articles"
-excluded_tickers = ['AAPL','TGS','BMA','EFC','MTGE','CRED','PEET']
+excluded_tickers = ['AAPL','BMA','CRED','CSCO','CVX','DMF',
+                    'DSM','EFC','FCX','LEO','MCC','MTGE',
+                    'PEET','TGS']
+
 
 class TickerList:
     def __init__(self, screen ):
